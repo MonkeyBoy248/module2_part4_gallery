050440b81505ed8928acf136a82c114aca0e5eed
Kirill Temnyi <kirik201100@mail.ru>
2022-03-31 18:15:14 +0300

    refactor: update config.ts import path
    MODULE-2-PART-4

--- backend/src/middleware/404_handler.ts
+++ backend/src/middleware/404_handler.ts
@@ -1,8 +1,8 @@
-import { Request , Response, NextFunction} from "express";
-import {paths} from "../../config";
+import { Request , Response } from "express";
+import {paths} from "../config";
 
 
-export function nonexistentPageHandler (req: Request, res: Response, next: NextFunction) {
+export function nonexistentPageHandler (req: Request, res: Response) {
   if (req.accepts('html')) {
     res.status(404).sendFile(paths.ERROR_PAGE_PATH);
     return;
