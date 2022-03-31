bfb473aac1227f7ba3d9f8da567f14b9ebcc97c7
Kirill Temnyi <kirik201100@mail.ru>
2022-03-31 18:16:21 +0300

    chore: rename object with authorized users according to camelCase style
    MODULE-2-PART-4

--- backend/src/authorized_users_list.ts
+++ backend/src/authorized_users_list.ts
@@ -1,6 +1,6 @@
 import {Authorized_user} from "./interfaces/authorized_user";
 
-export const authorized_users: Authorized_user = {
+export const authorizedUsers: Authorized_user = {
   'asergeev@flo.team':'jgF5tn4F',
   'vkotikov@flo.team':'po3FGas8',
   'tpupkin@flo.team':'tpupkin@flo.team'
