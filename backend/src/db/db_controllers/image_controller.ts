652efd58721553e90179b78683f6d00157eb6ed4
Kirill Temnyi <kirik201100@mail.ru>
2022-04-01 15:00:14 +0300

    feat: add pictures to the DB
    create addUserToDB function that check if this image is already in collection and save image object to DB if result of the check is negative
    
    MODULE-2-PART-4

---
+++ backend/src/db/db_controllers/image_controller.ts
@@ -0,0 +1,26 @@
+import { imageModel } from "../models/picture_model";
+import { getFileMetadata } from "../../utils/file_metadata";
+import { Pictures } from "../../utils/gallery_pictures";
+
+export async function addImagesToDB () {
+  const imageNames = await Pictures.getPictures() || [];
+  const imageMetadata = await getFileMetadata();
+
+  for (let i = 0; i < imageNames.length; i++) {
+    if (!await imageModel.exists({id: i})){
+      console.log('Collection is not empty');
+      return false;
+    }
+
+    await imageModel.create(
+      {
+        id: i,
+        path: imageNames[i],
+        metadata: imageMetadata[i],
+      }
+    )
+
+    console.log('All pictures added');
+  }
+}
+
