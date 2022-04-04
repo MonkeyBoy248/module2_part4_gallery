8a47dcf0327c2b130cd2b6e9ca2882053d0d8d03
Kirill Temnyi <kirik201100@mail.ru>
2022-04-04 15:42:42 +0300

    feat: import addImageToDB function
    after receiving picture file from client and renaming it send object with user picture id, new path and metadata to the DB
    
    MODULE-2-PART-4

--- backend/src/utils/filename_format.ts
+++ backend/src/utils/filename_format.ts
@@ -5,19 +5,32 @@ import { Pictures } from "./gallery_pictures";
 import {setDateFormat} from "./log_format";
 import { paths } from "../config";
 import {errorLog} from "./error_log";
+import {addUserImageToDB} from "../db/db_controllers/image_controller";
+
 
 
 export async function renameFile (req: Request, res: Response) {
   const pictureData = req.file;
-  const picturesLength = await Pictures.getPicturesLength();
+  const picturesLength = await Pictures.getPicturesAmount();
 
   try {
     if (pictureData) {
-      const pictureName = `image_${picturesLength}${pictureData.originalname.slice(pictureData.originalname.indexOf('.'))}`
+      const pictureName = `user_image_${picturesLength + 1}${pictureData.originalname.slice(pictureData.originalname.indexOf('.'))}`
 
       await fs.promises.rename(
         pictureData.path,
         path.join(paths.API_IMAGES_PATH, pictureName));
+
+      const metadata = await Pictures.getFileMetadata();
+
+
+      await addUserImageToDB(
+        {
+          id: `${picturesLength}_user`,
+          path: pictureName,
+          metadata: metadata[picturesLength]
+        }
+      );
 
       res.sendStatus(200);
     }
